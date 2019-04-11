b0dce569e5e5793469e814819ae40574ca0a54f1
richb999git <richb999@tiscali.co.uk>
2019-04-11 22:38:54 +0100

    AdminOrder controller captial letter change in web file

--- routes/web.php
+++ routes/web.php
@@ -55,46 +55,46 @@ Auth::routes();
 
 //############## ADMIN ROUTES ##############################################################################
 
-Route::post('/home', 'adminOrderController@showCurrentOrdersByName')->name('home')->middleware('auth');
-Route::get('/home', 'adminOrderController@showCurrentOrdersByName')->name('home')->middleware('auth');
+Route::post('/home', 'AdminOrderController@showCurrentOrdersByName')->name('home')->middleware('auth');
+Route::get('/home', 'AdminOrderController@showCurrentOrdersByName')->name('home')->middleware('auth');
 
-Route::get('/changePaymentStatusToPaid/{id}', 'adminOrderController@changePaymentStatusToPaid')
+Route::get('/changePaymentStatusToPaid/{id}', 'AdminOrderController@changePaymentStatusToPaid')
 ->name('changePaymentStatusToPaid')->middleware('auth');
 
-Route::get('/changePaymentStatusToUnpaid/{id}', 'adminOrderController@changePaymentStatusToUnpaid')
+Route::get('/changePaymentStatusToUnpaid/{id}', 'AdminOrderController@changePaymentStatusToUnpaid')
 ->name('changePaymentStatusToUnpaid')->middleware('auth');
 
-Route::get('/changeOrderStatusToRecd/{id}', 'adminOrderController@changeOrderStatusToRecd')
+Route::get('/changeOrderStatusToRecd/{id}', 'AdminOrderController@changeOrderStatusToRecd')
 ->name('changeOrderStatusToRecd')->middleware('auth');
 
-Route::get('/changeOrderStatusToCooking/{id}', 'adminOrderController@changeOrderStatusToCooking')
+Route::get('/changeOrderStatusToCooking/{id}', 'AdminOrderController@changeOrderStatusToCooking')
 ->name('changeOrderStatusToCooking')->middleware('auth');
 
-Route::get('/changeOrderStatusToDelivered/{id}', 'adminOrderController@changeOrderStatusToDelivered')
+Route::get('/changeOrderStatusToDelivered/{id}', 'AdminOrderController@changeOrderStatusToDelivered')
 ->name('changeOrderStatusToDelivered')->middleware('auth');
 
 // alternate syntax - no name so called using 'url' with a "/" instead of 'route' and the name
-Route::get('/changeOrderStatusToCollected/{id}', 'adminOrderController@changeOrderStatusToCollected')
+Route::get('/changeOrderStatusToCollected/{id}', 'AdminOrderController@changeOrderStatusToCollected')
 ->middleware('auth');
 
-Route::get('/changeOrderStatusToCancelled/{id}', 'adminOrderController@changeOrderStatusToCancelled')
+Route::get('/changeOrderStatusToCancelled/{id}', 'AdminOrderController@changeOrderStatusToCancelled')
 ->name('changeOrderStatusToCancelled')->middleware('auth');
 
-Route::post('/showUnpaidOrders', 'adminOrderController@showUnpaidOrders')->name('showUnpaidOrders')->middleware('auth');
-Route::get('/showUnpaidOrders', 'adminOrderController@showUnpaidOrders')->name('showUnpaidOrders')->middleware('auth');
+Route::post('/showUnpaidOrders', 'AdminOrderController@showUnpaidOrders')->name('showUnpaidOrders')->middleware('auth');
+Route::get('/showUnpaidOrders', 'AdminOrderController@showUnpaidOrders')->name('showUnpaidOrders')->middleware('auth');
 
-Route::get('/downloadOrders', 'adminOrderController@downloadOrders')->name('downloadOrders')->middleware('auth');
+Route::get('/downloadOrders', 'AdminOrderController@downloadOrders')->name('downloadOrders')->middleware('auth');
 
 
-Route::get('/showMenuItems', 'adminMenuController@showMenuItems')->name('showMenuItems')->middleware('auth');
-Route::get('/enterMenuItem', 'adminMenuController@enterMenuItem')->name('enterMenuItem')->middleware('auth');
-Route::post('/addMenuItem', 'adminMenuController@addMenuItem')->name('addMenuItem')->middleware('auth');
-Route::get('/editMenuItem/{id}', 'adminMenuController@editMenuItem')->name('editMenuItem')->middleware('auth');
-Route::patch('/updateMenuItem/{id}', 'adminMenuController@updateMenuItem')->name('updateMenuItem')->middleware('auth');
-Route::delete('/deleteMenuItem/{id}', 'adminMenuController@deleteMenuItem')->name('deleteMenuItem')->middleware('auth');
-Route::get('/downloadMenu', 'adminMenuController@downloadMenu')->name('downloadMenu')->middleware('auth');
-Route::get('/uploadMenu', 'adminMenuController@uploadMenu')->name('uploadMenu')->middleware('auth');
-Route::post('/uploadMenu', 'adminMenuController@importMenuCsv')->name('importMenuCsv')->middleware('auth');
+Route::get('/showMenuItems', 'AdminMenuController@showMenuItems')->name('showMenuItems')->middleware('auth');
+Route::get('/enterMenuItem', 'AdminMenuController@enterMenuItem')->name('enterMenuItem')->middleware('auth');
+Route::post('/addMenuItem', 'AdminMenuController@addMenuItem')->name('addMenuItem')->middleware('auth');
+Route::get('/editMenuItem/{id}', 'AdminMenuController@editMenuItem')->name('editMenuItem')->middleware('auth');
+Route::patch('/updateMenuItem/{id}', 'AdminMenuController@updateMenuItem')->name('updateMenuItem')->middleware('auth');
+Route::delete('/deleteMenuItem/{id}', 'AdminMenuController@deleteMenuItem')->name('deleteMenuItem')->middleware('auth');
+Route::get('/downloadMenu', 'AdminMenuController@downloadMenu')->name('downloadMenu')->middleware('auth');
+Route::get('/uploadMenu', 'AdminMenuController@uploadMenu')->name('uploadMenu')->middleware('auth');
+Route::post('/uploadMenu', 'AdminMenuController@importMenuCsv')->name('importMenuCsv')->middleware('auth');
 
 Route::resource('/menuCategories', 'AdminMenuCategoryController')->middleware('auth');
 Route::get('/downloadCategories', 'AdminMenuCategoryController@downloadCategories')->name('downloadCategories')->middleware('auth');
