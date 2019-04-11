8b3cf5fc3c776a1e3aef52d1eb6d5ba5a3f183c2
richb999git <richb999@tiscali.co.uk>
2019-04-11 23:12:38 +0100

    change to force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;  // I added this in to fix error: 1071 Specified key was too long; max key length is 767 bytes
+use Illuminate\Support\Facades\URL; // I added this based on https://dev.to/connor11528/deploy-a-laravel-5-app-to-heroku
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,13 @@ class AppServiceProvider extends ServiceProvider
     {
         // I added this in to fix error: 1071 Specified key was too long; max key length is 767 bytes
         Schema::defaultStringLength(191);
+
+        // not sure if the below is correct or in the correct method
+        // I added this based on https://dev.to/connor11528/deploy-a-laravel-5-app-to-heroku
+        // Force SSL in production
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
+
     }
 }
