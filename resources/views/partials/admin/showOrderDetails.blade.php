04afe233662080ed6ce442784e106cad519f6886
Richard Braunton <richard@computech.software>
2022-11-14 19:03:02 +0000

    Allow for no items ordered to prevent error

--- resources/views/partials/admin/showOrderDetails.blade.php
+++ resources/views/partials/admin/showOrderDetails.blade.php
@@ -3,11 +3,13 @@
 <span class="float-right">Order status: {{ ($order->status) }}</span><br>
 <span class="float-right">{{ $order->paid == "Y" ? "Paid for" : "Awaiting payment" }}</span><br>
 @php $total = 0; @endphp
-@foreach ( json_decode($order->p_order) as $orderLine)
-    <span>{{ ($orderLine->title) }}</span>
-    <span>£{{ number_format($orderLine->price, 2) }}</span><br>
-    @php $total += $orderLine->price @endphp
-@endforeach
+@if ($order->p_order !== "null")
+    @foreach ( json_decode($order->p_order) as $orderLine)
+        <span>{{ ($orderLine->title) }}</span>
+        <span>£{{ number_format($orderLine->price, 2) }}</span><br>
+        @php $total += $orderLine->price @endphp
+    @endforeach
+@endif
 <span><b>Total £{{ number_format($total, 2) }}</b></span>
 
 <br><br>
